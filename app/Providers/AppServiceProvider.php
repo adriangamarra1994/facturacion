00601baaf7cc636d90f0dbd9b1b02a157ef23cc0
unknown <adriangamarra1994@gmail.com>
2023-12-18 08:49:09 -0500

    pruebas

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,8 +19,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // if(config('app.debug')!=true) {
-        //     \URL::forceScheme('https');
-        //   }
+        if(config('app.debug')!=true) {
+            \URL::forceScheme('https');
+          }
     }
 }
--- resources/views/facturacion/companies/index.blade.php
+++ resources/views/facturacion/companies/index.blade.php
@@ -1,15 +1,16 @@
 <x-app-layout>
 
 
-    {{-- <div class="p-4 sm:mx-10 pt-20">
-        <div class="p-4 rounded-lg dark:border-gray-400 mt-14">           
+    <div class="p-4 sm:mx-10 pt-20">
+        {{-- <div class="p-4 rounded-lg dark:border-gray-400 mt-14">           
                <main>
                    @livewire('companies.company-index')
                </main>
-        </div>
-     </div> --}}
+        </div> --}}
+        Hola aqui deberia cargar
+     </div>
         
    
-     Hola aqui deberia cargar
+     
 
 </x-app-layout>
