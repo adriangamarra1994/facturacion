0799f107b3cc1bd03b61e1babf25bfc593ba9155
unknown <adriangamarra1994@gmail.com>
2023-12-18 12:43:33 -0500

    jjsjo

--- config/livewire.php
+++ config/livewire.php
@@ -27,7 +27,7 @@ return [
     */
 
     // 'view_path' => resource_path('views/livewire'),
-    'view_path' => resource_path('views/livewire'),
+    'view_path' => resource_path('https://sea-lion-app-l3672.ondigitalocean.app'),
 
     /*
     |---------------------------------------------------------------------------
