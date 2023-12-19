b012b17296017bc2c35aaba3897ab51c65d6e1f2
unknown <adriangamarra1994@gmail.com>
2023-12-19 08:37:35 -0500

    turu

---
+++ app/Livewire/EjemploIndex.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Company;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class EjemploIndex extends Component
+{
+
+    use WithPagination;
+    public $query;
+
+    public function updatingQuery()
+    {
+        $this->resetPage();
+    }
+    public function render()
+    {
+        $companies = Company::where('user_id', auth()->id())->get();
+        
+        return view('livewire.ejemplo-index', [
+            'companies' => Company::orderBy('id', 'DESC')
+                ->where('razon_social', 'like',  '%' . $this->query . '%')
+                ->paginate(5),
+        ]);
+    }
+}
