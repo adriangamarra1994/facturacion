7f002bda3bd9a3faa6f644d69cad9831759b1986
unknown <adriangamarra1994@gmail.com>
2023-12-19 13:12:55 -0500

    jsh

--- resources/views/facturacion/companies/index.blade.php
+++ resources/views/facturacion/companies/index.blade.php
@@ -4,7 +4,7 @@
     <div class="p-4 sm:mx-10 pt-20">
         <div class="p-4 rounded-lg dark:border-gray-400 mt-14">           
                <main>
-                   @livewire('companies.company-index')
+                   @livewire('company-index')
                    
                </main>
         </div>
